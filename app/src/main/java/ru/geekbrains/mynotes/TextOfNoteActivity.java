eac194971bd8b67a84bad3c29f5a79898ff8d6a3
kalinovdv <kdv_ul@rambler.ru>
2021-07-09 14:06:39 +0400

    Добавил Activity для просмотра текста заметки.

---
+++ app/src/main/java/ru/geekbrains/mynotes/TextOfNoteActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.mynotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextOfNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_of_note);
+    }
+}
